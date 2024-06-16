2c8e479e814e99f37f051b0a22f2114a369888db
Devon Hathaway <dhathaway4695@gmail.com>
2024-06-16 11:13:09 -0400

    Solution to Euler 1

---
+++ Euler1.py
@@ -0,0 +1,9 @@
+def sum_of_multiples(limit):
+    total = 0
+    for number in range(limit):
+        if number % 3 == 0 or number % 5 == 0:
+            total += number
+    return total
+
+result = sum_of_multiples(1000)
+print(result)
